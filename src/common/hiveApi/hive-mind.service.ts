0ec6d6d9ac9034373279521b2de44aa4f03cec12
Boris Andrzhiievskyi <borees2@gmail.com>
2021-09-30 16:15:37 +0300

    client inside try-catch

--- src/common/hiveApi/hive-mind.service.ts
+++ src/common/hiveApi/hive-mind.service.ts
@@ -3,13 +3,14 @@ import { Client } from '@hiveio/dhive';
 import { Post } from '../../post/interfaces/post.interface';
 import { RPC_NODES_HIVEMIND } from '../constants';
 
+const client = new Client(RPC_NODES_HIVEMIND, { timeout: 8000 });
+
 @Injectable()
 export class HiveMindService {
   private readonly logger = new Logger(HiveMindService.name);
 
   async getPost(author: string, permlink: string):Promise<Post> {
     try {
-      const client = new Client(RPC_NODES_HIVEMIND, { timeout: 8000 });
       return client.database.call('get_content', [author, permlink]);
     } catch (err) {
       this.logger.error(err.message);
