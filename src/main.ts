bbe2c434cf40eb7e8c79d5de82d875c986a9043c
fesmofet <borees2@gmail.com>
2022-09-26 18:03:54 +0300

    start rewriting

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { configService } from './common/config/config.service';
-import { AppModule } from './app.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(configService.getPort());
-}
-bootstrap();
--- src/redis/index.ts
+++
@@ -1,8 +0,0 @@
-import { RedisModule } from 'nestjs-redis';
-import { configService } from '../common/config/config.service';
-
-export const REDIS_IMPORTS = [
-  RedisModule.register([
-    configService.getRedisExpirePostConfig(),
-  ]),
-];
