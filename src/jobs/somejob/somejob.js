4a9a721ffcb0817fcb78bc9f96fc486eee07d22b
fesmofet <borees2@gmail.com>
2022-09-27 15:43:46 +0300

    add hive

--- src/jobs/somejob/somejob.js
+++ src/jobs/somejob/somejob.js
@@ -3,9 +3,8 @@ const { postModel } = require('../../database/models');
 const { redisGetter } = require('../../redis');
 
 const cacheHivePrice = new CronJob('*/10 * * * * *', async () => {
-  console.log('here');
   const kek = await redisGetter.get({ key: 'test' });
   const yo = await postModel.find({ filter: { author: 'ctrl-news' } });
   console.log();
-}, null, false, null, null, true);
+}, null, false, null, null, false);
 
---
+++ src/utilities/hiveEngine/accountHistory.js
@@ -0,0 +1,18 @@
+const axios = require('axios');
+const { REQUEST_TIMEOUT } = require('../../constants/common');
+
+exports.accountHistory = async (params) => {
+  try {
+    return {
+      response: await axios.get(
+        'https://accounts.hive-engine.com/accountHistory',
+        {
+          params,
+          timeout: REQUEST_TIMEOUT,
+        },
+      ),
+    };
+  } catch (error) {
+    return { error };
+  }
+};
