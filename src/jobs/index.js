f6b35b3c440e901d5596b746d42ec101932d26a0
fesmofet <borees2@gmail.com>
2023-07-31 11:07:11 +0300

    cron_return

--- src/jobs/index.js
+++ src/jobs/index.js
@@ -2,36 +2,23 @@ const {
   updatePostVotesJob,
   updatePostChildrenJob,
   waivioWelcomeJob,
-  apiCacheHivePrice,
-  apiCacheServiceBots,
-  apiCollectAppExperts,
-  apiCollectWobjectExperts,
-  apiImportUsers,
-  apiErroredUsers,
-  apiSendWebsiteDebt,
-  apiUpdateObjTypeExperts,
-  apiUpdateSiteObjects,
-  apiUpdateTopObjects,
-  apiUpdatePostsCount,
-  apiUpdateWaivioAdmins,
-  apiWebsiteBalanceNotification,
 } = require('./jobs');
 
 updatePostVotesJob.start();
 updatePostChildrenJob.start();
 waivioWelcomeJob.start();
 
-apiCacheHivePrice.start();
-apiCacheServiceBots.start();
+// apiCacheHivePrice.start();
+// apiCacheServiceBots.start();
 // for now we don't use it
 // apiCollectAppExperts.start();
-apiCollectWobjectExperts.start();
-apiImportUsers.start();
-apiErroredUsers.start();
-apiSendWebsiteDebt.start();
-apiUpdateObjTypeExperts.start();
-apiUpdateSiteObjects.start();
-apiUpdateTopObjects.start();
-apiUpdatePostsCount.start();
-apiUpdateWaivioAdmins.start();
-apiWebsiteBalanceNotification.start();
+// apiCollectWobjectExperts.start();
+// apiImportUsers.start();
+// apiErroredUsers.start();
+// apiSendWebsiteDebt.start();
+// apiUpdateObjTypeExperts.start();
+// apiUpdateSiteObjects.start();
+// apiUpdateTopObjects.start();
+// apiUpdatePostsCount.start();
+// apiUpdateWaivioAdmins.start();
+// apiWebsiteBalanceNotification.start();
